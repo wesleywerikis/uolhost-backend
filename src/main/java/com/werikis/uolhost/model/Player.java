9a08b1bd0e5f88a66a0ac9365b8d8efb5adfe97c
Wesley Werikis <wesleywerikis@outlook.com>
2023-12-12 14:15:36 -0300

    Added annotation to the player object

---
+++ src/main/java/com/werikis/uolhost/model/Player.java
@@ -0,0 +1,36 @@
+package com.werikis.uolhost.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "players")
+@Table(name = "players")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Player {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String email;
+    private String phoneNumber;
+    private String codiname;
+    private GroupType groupType;
+
+
+}
