1d7bba03430848e8ce771a63721336651afc8071
Wesley Werikis <wesleywerikis@outlook.com>
2023-12-12 17:42:36 -0300

    Add endpoint to get all players

--- src/main/java/com/werikis/uolhost/controller/PlayerController.java
+++ src/main/java/com/werikis/uolhost/controller/PlayerController.java
@@ -1,8 +1,11 @@
 package com.werikis.uolhost.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,4 +30,9 @@ public class PlayerController {
         return new ResponseEntity<>(newPlayer, HttpStatus.CREATED);
     }
     
+    @GetMapping
+    public ResponseEntity<List<Player>> getAllPlayers(){
+        return new ResponseEntity<>(playerService.getAllPlayers(), HttpStatus.OK);
+    }
+
 }
