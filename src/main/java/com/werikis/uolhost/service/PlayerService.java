a7675dfbc70460019969088ffbf7393391e30a0b
Wesley Werikis <wesleywerikis@outlook.com>
2023-12-12 17:35:09 -0300

    Add codename generation with CodenameHandler when creating players

--- src/main/java/com/werikis/uolhost/service/PlayerService.java
+++ src/main/java/com/werikis/uolhost/service/PlayerService.java
@@ -1,8 +1,12 @@
 package com.werikis.uolhost.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.werikis.uolhost.infra.CodinameHandler;
+import com.werikis.uolhost.model.GroupType;
 import com.werikis.uolhost.model.Player;
 import com.werikis.uolhost.model.dtos.PlayerDto;
 import com.werikis.uolhost.repostitory.PlayerRepository;
@@ -13,9 +17,23 @@ public class PlayerService {
     @Autowired
     private PlayerRepository playerRepository;
 
+    @Autowired
+    private CodinameHandler codinameHandler;
+
     public Player createPlayer(PlayerDto dto) {
         Player newPlayer = new Player(dto);
+        String codiname = getCodiname(dto.groupType());
+        newPlayer.setCodiname(codiname);
         return playerRepository.save(newPlayer);
     }
     
+    private String getCodiname(GroupType groupType){
+        return codinameHandler.findCodiname(groupType);
+    }
+
+    public List<Player> getAllPlayers(){
+        return playerRepository.findAll();
+    }
+
+
 }
