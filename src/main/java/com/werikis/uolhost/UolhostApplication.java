d7d4c229688fbc5f6b41865ecce774abe350ee00
Wesley Werikis <wesleywerikis@outlook.com>
2023-12-12 13:45:30 -0300

    Starting project

---
+++ src/main/java/com/werikis/uolhost/UolhostApplication.java
@@ -0,0 +1,13 @@
+package com.werikis.uolhost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UolhostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UolhostApplication.class, args);
+	}
+
+}
